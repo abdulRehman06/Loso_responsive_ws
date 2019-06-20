62029c1d8d50889a0a2f46dcb268c47c22fac396
abdulRehman06 <arkaimkhani06@gmail.com>
2019-06-21 01:03:45 +0500

    code for counter and libraries added for counter

--- js/custom.js
+++ js/custom.js
@@ -126,6 +126,15 @@ $(document).ready(function() {
 });
 
 
+// Add counter
+$(document).ready(function() {
+    
+    $('.counter-num').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
 
 
 
